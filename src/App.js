39192dabb443cc08c74f2d20d18f8a8a6fa8ee4d
Chris Kavanagh <ckava3@gmail.com>
2021-10-13 14:21:35 -0400

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./components/Home.page";
+import SuperHeroesPage from "./components/SuperHeroes.page";
+import RQSuperHeroesPage from "./components/RQSuperHeroes.page";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/SuperHeroes" element={<SuperHeroesPage />} />
+        <Route path="/RQSuperHeroes" element={<RQSuperHeroesPage />} />
+      </Routes>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/SuperHeroes">SuperHeroes</Link>
+        <Link to="/RQSuperHeroesPage">RQSuperHeroesPage</Link>
+      </nav>
     </div>
   );
 }
---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RQSuperHeroes() {
+  return <div>RQ Super Heroes Page</div>;
+}
